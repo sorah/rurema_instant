517d2a3fce1255f7a9bf6b250225ccda19c063bb
Shota Fukumori (sora_h) <sora134@gmail.com>
2010-09-21 09:01:45 +0900

    ver1.1

--- rurema_instant.safariextension/rurema_instant.js
+++ rurema_instant.safariextension/rurema_instant.js
@@ -13,13 +13,13 @@ function query_remap() {
         data: "query="+$("input.query").val(),
         cache: false,
         success: function(html){
-          console.log("haha");
+          location.hash = "#" + $("input.query").val();
           $("div.content").html(html.replace(/[\s\S]+<div class="content">([\s\S]*)<\/div>[\s\S]+<div class="footer">[\s\S]+/,"$1"));
         }
       });
     },300);
   });
-
+  $("input.query").focus();
 }
 if($(".search-header").length == 0) {
   $("#query-input").keyup(function(){
@@ -31,6 +31,7 @@ if($(".search-header").length == 0) {
         data: "query="+$("#query-input").val(),
         cache: false,
         success: function(html){
+          location.hash = "#" + $("#query-input").val();
           document.body.innerHTML = html;
           query_remap();
         }
@@ -39,5 +40,20 @@ if($(".search-header").length == 0) {
   });
 } else {
   query_remap();
-
+}
+if(location.hash != ""){
+  var q = location.hash.replace(/^#/,"");
+  $.ajax({
+    type: "POST",
+    url: "./",
+    data: "query="+q,
+    cache: false,
+    success: function(html){
+      document.body.innerHTML = html;
+      if($(".search-header").length == 0) {
+        query_remap();
+      }
+    }
+  });
+  $("#query-input, input.query").val(q);
 }
